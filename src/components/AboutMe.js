2a227bd955a6252d042e4f8725765cf4f0df25db
Daniel Bui <dnlbui@proton.me>
2023-03-27 12:02:19 -0500

    fix problem w/moble view and overscrolling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,7 +9,7 @@ function AboutMe() {
   }, []);
 
   return (
-    <div className="container" id="about" style={{ height: "100vh" }}>
+    <div className="container" id="about" style={{ minHeight: "100vh" }}>
       <div className="row align-items-center" style={{ height: "100vh" }}>
         <div className="col-md-4 ">
           <h2 className="heading">
@@ -26,32 +26,34 @@ function AboutMe() {
             My Resume
           </a>
         </div>
-        <div className="col-md-8" id="about-desc" data-aos="zoom-in-up">
+        <div className="col-md-8" id="about-desc" data-aos="fade-left">
           <p>
-            In 2020 I was curious about blockchain after listening to a podcast,
-            Crypto 101. Since then, smart contracts exposed me to decentralized
-            finance. What sparked my interest was how decentralized finance is
-            inclusive and opens opportunity in emerging and evolving established
-            markets.
+               In 2020 I was curious about blockchain after listening to a podcast,
+            Crypto 101. Since then, I've explored decentralized applications and
+            used smart contracts to expose me to decentralized finance. What
+            sparked my interest the most is how decentralized finance is
+            inclusive and opens opportunities in emerging and evolving
+            established markets.
           </p>
           <p>
-            With my new interest in finance, I joined Fidelity. I gained
-            valuable interpersonal skills at Fidelity. I assisted customers with
-            withdrawing 401k's, taking out loans, and quickly triaged urgent
+               With my new interest in finance, I joined Fidelity and gained
+            valuable interpersonal skills. I assisted an average of 48 customers
+            with withdrawing 401k's, taking out loans, and quickly triaged
             problems the customers presented. With this experience and
-            confidence gained. I felt the push to pursue my passion in
-            technology, where I've always been weary about until I started
+            confidence gained. I felt the push to pursue my passion for
+            technology, which I've always been weary about until I started
             networking and gained encouragement and advice.
           </p>
           <p>
-            In 2022, I attended Consensus, Algorand Developer Portal, and Reach
-            Summit. In 2023, I've been shadowing Shardeum developers and I
-            attend local developer meet-ups in Dallas. My goal is to get a
-            professional job in tech and provide value to an organization that
-            provides me with the opportunity.
+               In 2022, I attended Consensus, Algorand Developer Portal, and Reach
+            Summit. In 2023, I completed a MERN full-stack certificate.
+            Currently, I'm shadowing Shardeum developers, continuously
+            learning/practicing, and attending local developer meet-ups in
+            Dallas. My goal is to get a professional job in tech and provide
+            value to an organization that provides me with the opportunity.
           </p>
           <div className="arrow">
-            <Link rel="nofollow" to="projects" spy={true} smooth={true}>
+            <Link rel="nofollow" to="projects" spy={true} smooth={true} offset={150}>
               <span>
                 <i className="las la-angle-down la-3x"></i>
               </span>
